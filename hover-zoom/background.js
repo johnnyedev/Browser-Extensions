49538e0cf5f9d2efb2de7b78feb049a3fd605f15
johnnyedev <156373071+johnnyedev@users.noreply.github.com>
2025-01-28 17:53:53 -0600

    Update background.js

--- hover-zoom/background.js
+++ hover-zoom/background.js
@@ -1,21 +1,43 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.enabled !== undefined && message.tabId !== undefined) {
-    if (message.enabled) {
-      chrome.scripting.executeScript({
-        target: { tabId: message.tabId },
-        files: ['content.js']
-      });
-    } else {
-      chrome.scripting.executeScript({
-        target: { tabId: message.tabId },
-        func: () => {
-          // Disable content script functionality
-          if (window.contentScriptEnabled) {
-            window.contentScriptEnabled = false;
-	    window.location.reload();
-          }
+    chrome.tabs.get(message.tabId, function (tab) {
+      const url = new URL(tab.url);
+      const domain = url.hostname;
+
+      chrome.storage.local.get(['whitelist', 'blacklist', 'whitelistEnabled', 'blacklistEnabled'], function (result) {
+        const whitelistEnabled = result.whitelistEnabled;
+        const blacklistEnabled = result.blacklistEnabled;
+        const whitelist = result.whitelist || [];
+        const blacklist = result.blacklist || [];
+
+        if (whitelistEnabled && !whitelist.includes(domain)) {
+          console.log('Domain not in whitelist');
+          return;
+        }
+
+        if (blacklistEnabled && blacklist.includes(domain)) {
+          console.log('Domain in blacklist');
+          return;
+        }
+
+        if (message.enabled) {
+          chrome.scripting.executeScript({
+            target: { tabId: message.tabId },
+            files: ['content.js']
+          });
+        } else {
+          chrome.scripting.executeScript({
+            target: { tabId: message.tabId },
+            func: () => {
+              // Disable content script functionality
+              if (window.contentScriptEnabled) {
+                window.contentScriptEnabled = false;
+                // Add any cleanup code here if needed
+              }
+            }
+          });
         }
       });
-    }
+    });
   }
 });
